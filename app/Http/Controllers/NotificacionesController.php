f4af7ffc1117842e268bc3f02401efbe0510a2ab
SalomeVO <mvasquezo10@miug.edu.gt>
2023-04-21 09:33:44 -0600

    Tabla de Notificaciones terminada RD

---
+++ app/Http/Controllers/NotificacionesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\notificaciones;
+use Illuminate\Http\Request;
+
+class NotificacionesController extends Controller
+{
+
+    public function index()
+    {
+        $notificacion = notificaciones::paginate(10);//el numero de filas
+        return view('notificaciones.vistaNoti', compact('notificacion'));
+    }
+
+    public function deleteNoti($id_noti)
+    {
+        notificaciones::destroy($id_noti);
+        return redirect('/readnotificacion')->with('Eliminado', "Notificacion Enviada");
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\CompraController;
+use App\Http\Controllers\NotificacionesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -34,4 +35,6 @@ Route::delete('delatecliente/{nit}', [ClienteController::class,'deleteCliente'])
 /* Routes de Compras */
 Route::get('/readcompras',  [CompraController::class, 'index'])->name('index')->middleware('auth');;//Lista de Compras
 
-
+/* Routes de Notificaciones */
+Route::get('/readnotificacion',  [NotificacionesController::class, 'index'])->name('index')->middleware('auth');;//Lista de notificaciones
+Route::delete('delatenotificacion/{id_noti}', [NotificacionesController::class,'deleteNoti'])->name('deleteNoti')->middleware('auth'); //Eliminar una Notificacion
